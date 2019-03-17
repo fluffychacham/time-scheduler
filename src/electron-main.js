b5be88bc4807ced7b048ae2d7b8d6c994c6335be
fluffychacham <chamtejido@gmail.com>
2019-03-17 01:12:13 -0600

    fixed electron compatibility

--- src/electron-main.js
+++ src/electron-main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow(){
     let win = new BrowserWindow({
-        width: 800,
-        height: 900,
+        width: 1080,
+        height: 1920,
     });
 
     win.loadFile('index.html');
