dbc828d801a88c6f2594cecfa42606653f13c643
fluffychacham <chamtejido@gmail.com>
2019-03-17 01:29:17 -0600

    electron fixes

--- src/electron-main.js
+++ src/electron-main.js
@@ -4,8 +4,12 @@ function createWindow(){
     let win = new BrowserWindow({
         width: 1080,
         height: 1920,
+        minWidth: 1080,
+        minHeight: 1920
     });
 
+    win.webContents.openDevTools();
+    win.setMenu(null);
     win.loadFile('index.html');
 }
 
