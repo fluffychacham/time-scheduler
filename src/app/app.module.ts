597bc08d749f130452ae919118db44a6f4151453
fluffychacham <chamtejido@gmail.com>
2019-03-23 21:33:36 -0600

    removed bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { TopBarMenuComponent } from './top-bar-menu/top-bar-menu.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/top-bar-menu/top-bar-menu.component.ts
+++ src/app/top-bar-menu/top-bar-menu.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { 
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+ } from '@angular/animations';
 
 @Component({
   selector: 'app-top-bar-menu',
   templateUrl: './top-bar-menu.component.html',
-  styleUrls: ['./top-bar-menu.component.sass']
+  styleUrls: ['./top-bar-menu.component.sass'],
+  animations: [
+    trigger('navLinks',[
+      state('show', style({
+        height: 'auto'        
+      })),
+      state('hidden', style({
+        height: '0'
+      })),
+      transition('hidden => show', [
+        animate('.5s .5s ease-in-out')
+      ]),
+      transition('show => hidden', [
+        animate('.5s .5s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class TopBarMenuComponent implements OnInit {
 
   title = "Time Scheduler";
 
-  constructor() { }
+  username = "K";
+
+  showNavLinks = false;
+  showUserDropdown = false;
+
+  constructor() {}
+
+  @HostListener('document:click')
+  clickout(){
+  }
+
+  toggleNavLinks(){
+    this.showNavLinks = !this.showNavLinks;
+  }
+
+  toggleUserDropdown(){
+    this.showUserDropdown = !this.showUserDropdown;
+  }
 
   ngOnInit() {
   }
